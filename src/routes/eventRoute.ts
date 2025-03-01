40d738cd2cd4d81185619944cd8dd197be07cfed
vyut <vyut@msn.com>
2025-03-01 11:28:34 +0700

    Task 7 - การใช้ Error Handling - Try Catch

--- src/routes/eventRoute.ts
+++ src/routes/eventRoute.ts
@@ -9,14 +9,20 @@ router.get("/", async(req, res) => {
         const pageSize = parseInt(req.query.pageSize as string) || 3;
         const pageNo = parseInt(req.query.pageNo as string) || 1;
         const keyword = req.query.keyword as string;
-        const result = await service.getAllEventsWithPagination(keyword, pageSize, pageNo);
-        if (result.events.length === 0) {
-            res.status(404).send("No events found");
+
+        try {
+            const result = await service.getAllEventsWithPagination(keyword, pageSize, pageNo);
+            if (result.events.length === 0) {
+                res.status(404).send("No events found");
+                return;
+            }
+
+            res.setHeader("x-total-count", result.count.toString());
+            res.json(result.events);
+        } catch (error) {
+            res.status(500).send("Invalid Server Error");
             return;
         }
-
-        res.setHeader("x-total-count", result.count.toString());
-        res.json(result.events);
 
     } else if (req.query.category) {
         const category = req.query.category;
